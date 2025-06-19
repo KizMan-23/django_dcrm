7210ec8b174605a510b3e2a026ff481f52ccc2c9
KizMan-23 <kizitougwu777@gmail.com>
2025-06-19 01:31:11 +0100

    add templates and views

--- website/views.py
+++ website/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+
+def home(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        #authenticate user
+        user = authenticate(request, username= username, password= password)
+        if user is not None:
+            login(request, user=user)
+            messages.success(request, "Login Successful!")
+            return redirect('home')
+        else:
+            messages.success(request, "There is an error during LogIn")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'You have been logged Out..')
+    return redirect('home')
